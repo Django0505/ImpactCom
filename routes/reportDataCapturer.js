1d78a4f1f0a1ccf60b5633297173951db7979096
Emanuel <emanuel@projectcodex.co>
2015-10-07 16:23:55 +0200

    Answers saved nicely and template is no longer changed.

--- routes/reportDataCapturer.js
+++ routes/reportDataCapturer.js
@@ -23,30 +23,37 @@ module.exports = {
 	render_criteria : function(req, res, next){
 
 		var create_program_template = require('../data/funder_criteria');
-
+		console.log(create_program_template)
 		res.render("criteria", {create_program_template : create_program_template});
 	},
 	save_report : function(req, res, next){
-		var report_template = require('../data/funder_criteria.json'),
-			report_answers = JSON.parse(JSON.stringify(req.body));
+		var report_template = require('../data/funder_criteria'),
+			answers = JSON.parse(JSON.stringify(req.body));
 
+		var report_answers = {};
+
+		for(key in report_template){
+			report_answers[key] = report_template[key]; 
+		}
 		//Getting basic hub details
-		report_template.OrganisationName = report_answers.OrganisationName;
-		report_template.ContactPerson = report_answers.ContactPerson;
-		report_template.Type = report_answers.Type;
-		report_template.Description = report_answers.Description;
+		report_answers.OrganisationName = answers.OrganisationName;
+		report_answers.ContactPerson = answers.ContactPerson;
+		report_answers.Type = answers.Type;
+		report_answers.Description = answers.Description;
 
-		// console.log(report_answers)
 		//Getting Criteria values
-		for(var metric = 0; metric < report_template.Criteria.length; metric++){
-			if(report_answers.hasOwnProperty(report_template.Criteria[metric].fieldName)){
-				report_template.Criteria[metric].Value = report_answers[report_template.Criteria[metric].fieldName];
+		for(var metric = 0; metric < report_answers.Criteria.length; metric++){
+
+			if(answers.hasOwnProperty(report_answers.Criteria[metric].fieldName)){
+
+				report_answers.Criteria[metric].Value = answers[report_answers.Criteria[metric].fieldName];
+
 			}
 		}
 
 		// Removing spaces from OrganisationName
-		var OrganisationName = report_answers.OrganisationName.replace(/\s/g, '_');
-		save_data(report_template, '../data/'+OrganisationName+'_criteria_answers.json');
+		var OrganisationName = answers.OrganisationName.replace(/\s/g, '_');
+		save_data(report_answers, '../data/'+OrganisationName+'_criteria_answers.json');
 
 		res.redirect('/criteria')
 
